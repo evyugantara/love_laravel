0f96973735f8c5661d1cd3b01f0cad32fd3062f0
Ega Virga Yugantara <egavirga01@email.com>
2026-02-25 03:26:41 +0700

    Day 1

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ContactController</title>
+</head>
+<body>
+    INI HALAMAN CONTACT
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\RiwayatController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello', function () {
+    return ('Hello');
+});
+
+Route::get('/beranda', [BerandaController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/produk', [ProdukController::class, 'index']);
+Route::get('/riwayat', [RiwayatController::class, 'index']);
+
